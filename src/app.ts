31b9ec357213d139f94c139734dc8a73c16cf4f2
Atul <atul007414@gmail.com>
2026-05-02 21:24:32 +0530

    Added GET/ endpoint

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('School Management API is running');
+});
+
 app.use('/api/v1', schoolRoutes)
 
 export default app;
